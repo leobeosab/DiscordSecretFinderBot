e61e1c4b1274a6f36f409170e893147336c95187
Ryan Wise <ryan@ryanwise.me>
2019-06-26 18:13:17 -0400

    changed location of config file

--- modules/helper.py
+++ modules/helper.py
@@ -15,7 +15,7 @@ def get_github_string(data):
 
 # link needs to be https://github.com/user/repo.git
 def run_truffle_hog(githublink):
-    subprocessResult = subprocess.run(['trufflehog', '--include_paths', 'include-patterns.config', '--regex', '--entropy=True', githublink], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    subprocessResult = subprocess.run(['trufflehog', '--include_paths', 'config/include-patterns.config', '--regex', '--entropy=True', githublink], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = subprocessResult.stdout.decode('utf-8')
 
     if 'Repository not found' in output:
